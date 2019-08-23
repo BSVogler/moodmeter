816e834fe54751d8171e1df7f7bd1c564cd81931
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2019-08-23 18:12:59 +0200

    longer hash

--- moodbarometer/ModelController.swift
+++ moodbarometer/ModelController.swift
@@ -63,11 +63,13 @@ class ModelController: NSObject, UIPageViewControllerDataSource {
 	
 	final func generateSharingURL(){
 		//the hash does not have to be secure, just the seed, so use secure seed directly
-		var bytes = [UInt8](repeating: 0, count: 10)
+		var bytes = [UInt8](repeating: 0, count: 40)
 		let status = SecRandomCopyBytes(kSecRandomDefault, bytes.count, &bytes)
-		
+
 		if status == errSecSuccess { // Always test the status.
-			deviceHash = String(bytes: bytes, encoding: .ascii) ?? "failed"
+			let letters: [Character] = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+			let toURL: String = String(bytes.map{ byte in letters[Int(byte % UInt8(letters.count))] })
+			deviceHash = toURL
 		}
 	}
 
