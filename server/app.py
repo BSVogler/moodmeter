aedebb9224952c3664326b7c53a043d41718466c
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2019-08-25 17:17:43 +0200

    create folders automatically

--- server/app.py
+++ server/app.py
@@ -7,8 +7,18 @@ import hashlib
 
 app = Flask(__name__)
 
+password_folder = "passwords/"
+userdata_folder = "userdata/"
+
 
 def measurements_json_to_csv(fp, measurements, limit=None):
+    """
+    Transform python/json to csv
+    :param fp: filepointer
+    :param measurements: data
+    :param limit: filter dates
+    :return:
+    """
     for e in measurements:
         if limit is None:
             fp.write(e["day"]+";"+str(e["mood"])+";\n")
@@ -18,6 +28,7 @@ def measurements_json_to_csv(fp, measurements, limit=None):
                 fp.write(e["day"] + ";" + str(e["mood"]) + ";\n")
             else:
                 print("Invalid date")
+
 
 @app.route('/<string:repohash>', methods=['POST', 'GET'])
 def add_data(repohash):
@@ -30,11 +41,11 @@ def add_data(repohash):
         if not request.json:
             return abort(400)
         data = request.json["data"]
-        # check if exists
-        if os.access("userdata/"+repohash, os.R_OK):
+        # check if exists, then add
+        if os.access(userdata_folder + repohash, os.R_OK):
             # check password
-            if os.access("passwords/" + repohash, os.R_OK):
-                with open("passwords/" + repohash, "r") as pfp:
+            if os.access(password_folder + repohash, os.R_OK):
+                with open(password_folder + repohash, "r") as pfp:
                     stored_hash = pfp.readline()
                 transmitted_hash = hashlib.sha224(data["password"].encode('utf-8')).hexdigest()
                 if stored_hash != transmitted_hash:
@@ -42,10 +53,10 @@ def add_data(repohash):
             else:
                 return abort(Response("Password not found."))
 
-            with open("userdata/"+repohash, "a") as fp:
+            with open(userdata_folder + repohash, "a") as fp:
                 # check if date is allowed.
                 last_entry = None
-                with open("userdata/"+repohash, 'r') as f:
+                with open(userdata_folder + repohash, 'r') as f:
                     lines = f.read().splitlines()
                     last_line = lines[-1]
                 datestring = last_line[:last_line.find(';')]
@@ -53,17 +64,18 @@ def add_data(repohash):
 
                 measurements_json_to_csv(fp, data["measurements"], last_entry)
         else:
+            #create new entries
             transmitted_hash = hashlib.sha224(data["password"].encode('utf-8')).hexdigest()
-            with open("passwords/" + repohash, "w") as fp:
+            with open(password_folder + repohash, "w") as fp:
                 fp.write(transmitted_hash)
 
-            with open("userdata/"+repohash, "w") as fp:
+            with open(userdata_folder + repohash, "w") as fp:
                 measurements_json_to_csv(fp, data["measurements"])
 
         return "ok"
     else:
-        if os.access("userdata/"+repohash, os.R_OK):
-            with open("userdata/"+repohash, "r") as fp:
+        if os.access(userdata_folder + repohash, os.R_OK):
+            with open(userdata_folder + repohash, "r") as fp:
                 # todo check password properly
                 #if data["password"] != "empty":
                     csvdata = fp.read()
@@ -76,4 +88,8 @@ def add_data(repohash):
 
 
 if __name__ == '__main__':
+    if not os.path.isdir(password_folder):
+        os.makedirs(password_folder)
+    if not os.path.isdir(userdata_folder):
+            os.makedirs(userdata_folder)
     app.run()
