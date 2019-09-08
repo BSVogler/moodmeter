ce5d00874804ad7261360df117295954ccd9c5e2
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2019-09-08 13:51:38 +0200

    changed colors

--- moodbarometer/FaceViewController.swift
+++ moodbarometer/FaceViewController.swift
@@ -17,7 +17,7 @@ class FaceViewController: UIViewController {
 	var modelController: PageViewController?
 	
 	private var moodToText: [String] = ["?", ":-(", ":-/", ":-|", ":-)", ":-D"]
-	private var moodToColor: [UIColor] = [#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1), #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)]
+	private var moodToColor: [UIColor] = [#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 0.6324028457, green: 0.5172401532, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.5333333333, green: 1, blue: 0.2784313725, alpha: 1), #colorLiteral(red: 1, green: 0.8941176471, blue: 0.1490196078, alpha: 1)]
 	
 	//do not like this construct, but computed properties are only working this way
 	private var dateWithoutHours: Date = Date()
