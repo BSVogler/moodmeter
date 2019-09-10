f29fc1f3c8d6e1216cccad234e8e767ac88aa05c
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2019-09-10 12:54:04 +0200

    same probability for letters or numerals

--- moodmeter/Model/Model.swift
+++ moodmeter/Model/Model.swift
@@ -55,7 +55,7 @@ class Model {
 		let status = SecRandomCopyBytes(kSecRandomDefault, bytes.count, &bytes)
 		
 		if status == errSecSuccess { // Always test the status.
-			let letters: [Character] = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+			let letters: [Character] = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
 			let toURL: String = String(bytes.map{ byte in letters[Int(byte % UInt8(letters.count))] })
 			Model.shared.deviceHash = toURL
 		}
