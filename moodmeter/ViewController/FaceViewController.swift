72c259c97d1601126fa40e270b3a17a94580532f
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2019-09-13 18:30:54 +0200

    remove outdated code

--- moodmeter/ViewController/FaceViewController.swift
+++ moodmeter/ViewController/FaceViewController.swift
@@ -75,11 +75,6 @@ class FaceViewController: UIViewController {
 	}
 	
 	// MARK: - Overrides
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-	}
-
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		self.dataLabel!.text = topLabel
