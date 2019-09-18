57d06452e018ad5683aae520e61c08d970c6b014
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2019-09-18 23:14:51 +0200

    can generate a new link

--- moodmeterwatch WatchKit Extension/InterfaceController/ShareInterfaceController.swift
+++ moodmeterwatch WatchKit Extension/InterfaceController/ShareInterfaceController.swift
@@ -13,6 +13,21 @@ class ShareInterfaceController: WKInterfaceController {
 	
 	@IBOutlet weak var hashLabel: WKInterfaceLabel!
 	
+	@IBAction func generateNewLink() {
+		if let oldHash = Model.shared.deviceHash {
+			hashLabel.setText("...wait...")
+			//generate new sharing url
+			Model.shared.generateSharingURL()
+			MoodAPIjsonHttpClient.shared.moveHash(old: oldHash) { res in
+				self.hashLabel.setText(Model.shared.sharingURL?.absoluteString)
+			}
+		} else {
+			//has no hash (only the case if there is a bug somehwere else), so make a new one
+			Model.shared.generateAndRegisterSharingURL(){ res in
+				self.hashLabel.setText(Model.shared.sharingURL?.absoluteString)
+			}
+		}
+	}
 	override func awake(withContext context: Any?) {
 		if Model.shared.sharingURLShort != nil {
 			hashLabel.setText(Model.shared.sharingURLShort)
