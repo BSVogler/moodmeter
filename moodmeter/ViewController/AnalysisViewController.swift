52bba3e305652abaee04833c7539cca09155413a
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2019-09-19 08:31:12 +0200

    move analysisi to history view

--- moodmeter/ViewController/AnalysisViewController.swift
+++ moodmeter/ViewController/AnalysisViewController.swift
@@ -11,19 +11,5 @@ import SwiftChartView
 
 class AnalysisViewController: UIViewController {
 
-	@IBOutlet weak var diagram: LineChartView!
-	private lazy var chartPoints: [ChartPoint] = {
-		var chartPoints: [ChartPoint] = []
-		let moods = Model.shared.measurements.map{$0.mood}
-		for i in 0 ..< moods.count {
-			let chartPoint = ChartPoint(label: "11-0\(i+1)", value: Double(moods[i])/5.0)
-			chartPoints.append(chartPoint)
-		}
-		return chartPoints
-	}()
-	
-	override func viewDidLoad() {
-		diagram.chartPoints = chartPoints
-		diagram.pointStyle = .circle
-	}
+
 }
--- moodmeter/ViewController/HistoryViewController.swift
+++ moodmeter/ViewController/HistoryViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class HistoryViewController: UIViewController {
 
-	@IBOutlet weak var historyLabel: UILabel!
+	@IBOutlet weak var diagram: LineChartView!
+	private lazy var chartPoints: [ChartPoint] = {
+		var chartPoints: [ChartPoint] = []
+		let moods = Model.shared.measurements.map{$0.mood}
+		for i in 0 ..< moods.count {
+			let chartPoint = ChartPoint(label: "11-0\(i+1)", value: Double(moods[i])/5.0)
+			chartPoints.append(chartPoint)
+		}
+		return chartPoints
+	}()
 	
 	func refreshRendering(){
 		let sortedDates = Model.shared.dataset.keys.sorted(by: {$0.compare($1) == .orderedDescending})
@@ -22,6 +31,11 @@ class HistoryViewController: UIViewController {
 			return formatter.string(from:$0)+" "+(Model.shared.dataset[$0]?.getSmiley() ?? "")
 		}
 		historyLabel.text = lines.joined(separator: "\n")
+	}
+	
+	override func viewDidLoad() {
+		diagram.chartPoints = chartPoints
+		diagram.pointStyle = .circle
 	}
 	
 	override func viewWillAppear(_ animated: Bool) {
