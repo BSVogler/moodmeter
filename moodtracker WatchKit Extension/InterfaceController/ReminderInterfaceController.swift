03b732da9cde0b89296ad0e5651abb99f89c4450
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2019-09-26 20:54:16 +0200

    ask for permissions and send basic notification
    #39

--- moodtracker WatchKit Extension/InterfaceController/ReminderInterfaceController.swift
+++ moodtracker WatchKit Extension/InterfaceController/ReminderInterfaceController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import WatchKit
+import UserNotifications
 
 class ReminderInterfaceController: WKInterfaceController {
 	
@@ -31,7 +32,6 @@ class ReminderInterfaceController: WKInterfaceController {
 		if Model.shared.reminderEnabled {
 			Model.shared.reminderHour = value
 			registerNotification()
-			_ = Model.shared.saveToFiles()
 		}
 	}
 	
@@ -39,13 +39,13 @@ class ReminderInterfaceController: WKInterfaceController {
 		if Model.shared.reminderEnabled {
 			Model.shared.reminderMinute = value
 			registerNotification()
-			_ = Model.shared.saveToFiles()
 		}
 	}
 	
 	override init() {
 		super.init()
 		notificationSwitch.setOn(Model.shared.reminderEnabled)
+		reminderTimePicker.setHidden(!Model.shared.reminderEnabled)
 		let hours = (0...23).map{ value -> WKPickerItem in
 			let item = WKPickerItem()
 			item.title = String(value)
@@ -63,38 +63,42 @@ class ReminderInterfaceController: WKInterfaceController {
 	}
 	
 	// MARK: Instance Methods
-	func registerNotification(){
+	func registerNotification() {
 		var dateComponents = DateComponents()
 		dateComponents.calendar = Calendar.current
 		dateComponents.hour = Model.shared.reminderHour
 		dateComponents.minute = Model.shared.reminderMinute
-		//		let content = UNMutableNotificationContent()
-		//		content.title = NSLocalizedString("Mood Time", comment: "")
-		//		content.body = NSLocalizedString("It is time to give me your mood.", comment: "")
-		//		// Create the trigger as a repeating event.
-		//		let trigger = UNCalendarNotificationTrigger(
-		//			dateMatching: dateComponents, repeats: true)
-		//
-		//		// Create the request
-		//		let uuidString = UUID().uuidString
-		//		let request = UNNotificationRequest(identifier: uuidString,
-		//											content: content, trigger: trigger)
-		//
-		//		// Schedule the request with the system.
-		//		let notificationCenter = UNUserNotificationCenter.current()
-		//		notificationCenter.removeAllPendingNotificationRequests()
-		//		notificationCenter.add(request) { (error) in
-		//			if error != nil {
-		//				print(error ?? "could not register notification")
-		//			}
-		//		}
+		let content = UNMutableNotificationContent()
+		content.title = NSLocalizedString("Mood Time", comment: "")
+		content.body = NSLocalizedString("It is time to give me your mood.", comment: "")
+		// Create the trigger as a repeating event.
+		let trigger = UNCalendarNotificationTrigger(
+			dateMatching: dateComponents, repeats: true)
+		
+		// Create the request
+		let uuidString = UUID().uuidString
+		let request = UNNotificationRequest(identifier: uuidString,
+											content: content, trigger: trigger)
+		
+		// Schedule the request with the system.
+		let notificationCenter = UNUserNotificationCenter.current()
+		notificationCenter.removeAllPendingNotificationRequests()
+		notificationCenter.add(request) { (error) in
+			if error != nil {
+				print(error ?? "could not register notification")
+			}
+		}
 	}
 	
 	func registerNotificationRights() {
-		//	  UNUserNotificationCenter.current() // 1
-		//		.requestAuthorization(options: [.alert, .sound, .badge]) { // 2
-		//		  granted, error in
-		//		  print("Permission granted: \(granted)") // 3
-		//	  }
+		UNUserNotificationCenter.current() // 1
+			.requestAuthorization(options: [.alert, .sound, .badge]) { // 2
+				granted, error in
+				print("Permission granted: \(granted)") // 3
+		}
+	}
+	
+	deinit {
+		_ = Model.shared.saveToFiles()
 	}
 }
