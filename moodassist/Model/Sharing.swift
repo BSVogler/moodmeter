71ecbb44c84fda70349575d7beb831bdcf5ee485
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2019-10-01 07:32:02 +0200

    remove length restriction

--- moodassist/Model/Sharing.swift
+++ moodassist/Model/Sharing.swift
@@ -12,13 +12,6 @@ import Alamofire
 
 // MARK: Sharing
 class Sharing: Codable {
-	
-	//https://en.wikipedia.org/wiki/Birthday_attack
-	//with alphabet of 34 there are ~3*10^20 possibilities. Birthday paradoxon colission probability is aprox 10^-8 after 2.4 Mio tries
-	//for 5 107 billion tries are needed
-	static let hashlength = 10;
-	static let alphabet: [Character] = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
-	
 	//MARK: Stored Instance Properties
     let baseURL = NSURL(string: "https://api.moodassist.cloud")! as URL
 	private(set) var userHash: String? {
@@ -85,7 +78,7 @@ class Sharing: Codable {
 			//this should not happen
 			return
 		}
-		if Sharing.hashlength == hash.count {
+		if hash.count > 1 {
 			if let old = self.userHash {
 				MoodApiJsonHttpClient.shared.importHash(old: old, new: hash) { res in
 					if res.isSuccess {
