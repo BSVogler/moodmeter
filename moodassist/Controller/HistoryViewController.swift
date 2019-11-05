df583bcae997ca93d3921ebc0c234f628961b2b3
Benedikt S. Vogler <benedikt.s.vogler@tum.de>
2019-11-05 19:46:53 +0100

    add request for review
    #58

--- moodassist/Controller/HistoryViewController.swift
+++ moodassist/Controller/HistoryViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import StoreKit
 
 class HistoryViewController: UIViewController {
 	
@@ -26,21 +27,25 @@ class HistoryViewController: UIViewController {
 			diagramController.analysisrange = .year
 		}
 		refreshRendering()
+		actionToShowReview()
 	}
 	
 	@IBAction func navigateForward(_ sender: Any) {
 		diagramController.navigateForward()
 		refreshRendering()
+		actionToShowReview()
 	}
 	
 	@IBAction func navigateBackwards(_ sender: Any) {
 		diagramController.navigateBack()
 		refreshRendering()
+		actionToShowReview()
 	}
 	
 	// MARK: stored properties
 	let diagramController = DiagramController()
 	private let diagram : Diagram
+	private var canCreateNewReview = true
 	
 	// MARK: initializers
 	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -73,5 +78,35 @@ class HistoryViewController: UIViewController {
 		//causes a correct size redraw #49
 		diagram.frame = diagramImage.frame
 		refreshRendering()
+	}
+	
+	///call this when an action could potentially request a review
+	private func actionToShowReview(){
+		var count = UserDefaults.standard.integer(forKey: "processCompletedCount")
+		count += 1
+		UserDefaults.standard.set(count, forKey: "processCompletedCount")
+
+		// Has the process been completed several times and the user is semi-experienced?
+		if count >= 3 && (Model.shared.measurements.count > 2) {
+			// and the user has not already been prompted for this version?
+			// Get the current bundle version for the app
+			let infoDictionaryKey = kCFBundleVersionKey as String
+			guard let currentVersion = Bundle.main.object(forInfoDictionaryKey: infoDictionaryKey) as? String
+				else { fatalError("Expected to find a bundle version in the info dictionary") }
+			let lastVersionPromptedForReview = UserDefaults.standard.string(forKey: "lastVersionPromptedForReview")
+			if currentVersion != lastVersionPromptedForReview {
+				if canCreateNewReview { // only show a single one until completion
+					canCreateNewReview = false
+					let secondsFromNow = DispatchTime.now() + 1.5
+					DispatchQueue.main.asyncAfter(deadline: secondsFromNow) {
+						SKStoreReviewController.requestReview()
+						UserDefaults.standard.set(currentVersion, forKey: "lastVersionPromptedForReview")
+						//don't spam imediately after each udpate
+						UserDefaults.standard.set(0, forKey: "processCompletedCount")
+						self.canCreateNewReview = true
+					}
+				}
+			}
+		}
 	}
 }
